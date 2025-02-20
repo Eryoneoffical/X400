924db9ceab04d64a884031057e1a1579114af93d
robert <robert@eryone.com>
2025-02-20 15:18:52 +0800

    init

--- all/git_pull.sh
+++ all/git_pull.sh
@@ -9,11 +9,11 @@
 #echo makerbase | sudo -S service KlipperScreen restart
 
 
-#rm  /home/mks/KlipperScreen  -rf
-#cd  /home/mks/
-#git clone https://gitee.com/xpp123/KlipperScreen.git
-cd  /home/mks/KlipperScreen
-git fetch --all &&  git reset --hard origin/master && git pull
+rm  /home/mks/KlipperScreen  -rf
+cd  /home/mks/
+git clone  https://gitee.com/everyone3d/KlipperScreen.git
+#cd  /home/mks/KlipperScreen
+#git fetch --all &&  git reset --hard origin/master && git pull
 chmod 777 /home/mks/KlipperScreen/all/relink_conf.sh
 /home/mks/KlipperScreen/all/relink_conf.sh
 sync
