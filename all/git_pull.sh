6cf279c628aa6c8bc4af18d20d3f07d24c467c0d
robert <niujl123@sina.com>
2025-02-22 13:39:25 +0800

    3

--- all/git_pull.sh
+++ all/git_pull.sh
@@ -16,6 +16,7 @@ cd  /home/mks/KlipperScreen
 rm -f .git/index
 git reset
 git fetch --all &&  git reset --hard origin/master && git pull
+chmod 777 /home/mks/KlipperScreen/* -Rf
 chmod 777 /home/mks/KlipperScreen/all/relink_conf.sh
 /home/mks/KlipperScreen/all/relink_conf.sh
 sync
